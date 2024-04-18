670d4690f8fbf33f2cefcc0acc4e456e76657c61
Towobolar <71108083+Towobolar@users.noreply.github.com>
2024-04-18 18:27:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ provider "aws" {
 /***************************
 *     create vpc          *
 ***************************/
-
+          
 resource "aws_vpc" "three-tier-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
