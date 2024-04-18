537b6f29233c8226e623010cc4b9ff0c1e3c85db
Towobolar <71108083+Towobolar@users.noreply.github.com>
2024-04-18 18:20:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,10 @@
    #     value = "A example resource that does nothing!"
   #    }
    # }
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+}
 /***************************
 *     create vpc          *
 ***************************/
